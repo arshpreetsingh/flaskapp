c3eccff1ae9af97b92f4eeab930d4a692419f67b
arsh <honeykhangura@csiom.com>
2016-02-15 13:38:05 +0530

    just commit

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ def week_graph_data():
     
     try:
         dates_outbox = tuple(b[:-7] for b in outbox_week())
-        dates_inbox = tuple(a[:-7] for a in inbox_week())
+       # dates_inbox = tuple(a[:-7] for a in inbox_week())
     
         week_list = week_list_func()
     
@@ -205,7 +205,7 @@ def week_graph_data():
     
         bar_chart.add('Sent', [sum(t==i for t in dates_outbox) for i in week_list])
     
-        bar_chart.add('Received', [sum(m==n for n in dates_inbox) for m in week_list])
+        #bar_chart.add('Received', [sum(m==n for n in dates_inbox) for m in week_list])
     
         week_chart = bar_chart.render(is_unicode = True)
     
